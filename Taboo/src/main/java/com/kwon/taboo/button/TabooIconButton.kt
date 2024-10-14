d625f74e0123d1e197c910ddba0b654300955b66
kwonhanjun <skclaqks11@naver.com>
2024-10-14 20:51:02 +0900

    TABOO-3 TabooIconButton setBackground 메소드 삭제

--- Taboo/src/main/java/com/kwon/taboo/button/TabooIconButton.kt
+++ Taboo/src/main/java/com/kwon/taboo/button/TabooIconButton.kt
@@ -22,8 +22,6 @@ private const val ICON_POSITION_RIGHT = 1
 class TabooIconButton(context: Context, attrs: AttributeSet): ConstraintLayout(context, attrs) {
     private val binding = TabooIconButtonBinding.inflate(LayoutInflater.from(context), this, true)
 
-    private var background = R.drawable.selector_taboo_icon_button
-
     private var enabled = true
 
     private var text = ""
@@ -38,7 +36,7 @@ class TabooIconButton(context: Context, attrs: AttributeSet): ConstraintLayout(c
 
     init {
         val typed = context.obtainStyledAttributes(attrs, R.styleable.TabooIconButton)
-        val background = typed.getResourceId(R.styleable.TabooIconButton_android_background, 0)
+        val background = typed.getResourceId(R.styleable.TabooIconButton_android_background, R.drawable.selector_taboo_icon_button)
         val enabled = typed.getBoolean(R.styleable.TabooIconButton_android_enabled, true)
         val text = typed.getString(R.styleable.TabooIconButton_android_text) ?: ""
         val textColor = typed.getColorStateList(R.styleable.TabooIconButton_android_textColor)
@@ -49,7 +47,7 @@ class TabooIconButton(context: Context, attrs: AttributeSet): ConstraintLayout(c
 
         typed.recycle()
 
-        setBackground(background)
+        setBackground(ContextCompat.getDrawable(context, background))
         setEnabled(enabled)
         setText(text)
         setTextColor(textColor)
@@ -60,18 +58,6 @@ class TabooIconButton(context: Context, attrs: AttributeSet): ConstraintLayout(c
 
         invalidate()
     }
-
-    fun setBackground(background: Int) {
-        if (background == 0) return
-
-        this.background = background
-        updateBackground()
-    }
-
-    private fun updateBackground() {
-        binding.root.background = ContextCompat.getDrawable(context, background)
-    }
-
 
     override fun setEnabled(enabled: Boolean) {
         super.setEnabled(enabled)
@@ -188,7 +174,6 @@ class TabooIconButton(context: Context, attrs: AttributeSet): ConstraintLayout(c
     override fun invalidate() {
         super.invalidate()
 
-        updateBackground()
         updateEnabled()
         updateText()
         updateIconDrawable()
