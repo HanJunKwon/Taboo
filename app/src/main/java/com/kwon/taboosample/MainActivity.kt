81641496de99c30205239cfb046287840e5502a7
kwonhanjun <skclaqks11@naver.com>
2024-12-17 20:45:25 +0900

    TabooPreviewButton 추가

--- app/src/main/java/com/kwon/taboosample/MainActivity.kt
+++ app/src/main/java/com/kwon/taboosample/MainActivity.kt
@@ -4,17 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.kwon.taboo.button.TabooIconButton
+import com.kwon.taboo.button.TabooPreviewButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TabooIconButton>(R.id.btn_taboo_text_button).apply {
-            setOnClickListener {
-                Log.d(">>>", "Taboo text button")
-                this.isEnabled = !isEnabled
-            }
+        findViewById<TabooPreviewButton>(R.id.btn_preview).setOnClickListener {
+            Log.d(">>>", "buttonClick")
         }
     }
 }
