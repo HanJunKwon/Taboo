eace6bf5e243e304fbe7dee5176d6f34a2447c0d
kwonhanjun <skclaqks11@naver.com>
2025-01-06 20:47:38 +0900

    TABOO-5 TabooEditText 추가

---
+++ Taboo/src/main/java/com/kwon/taboo/edittext/TabooEditText.kt
@@ -0,0 +1,150 @@
+package com.kwon.taboo.edittext
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.kwon.taboo.R
+import com.kwon.taboo.databinding.TabooEditTextBinding
+
+class TabooEditText(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+    private val binding = TabooEditTextBinding.inflate(LayoutInflater.from(context), this, true)
+
+    private var prefixText: String = ""
+    private var prefixTextAppearance = R.style.Taboo_TextAppearance_TabooEditText_Affix
+    private var prefixTextColor: ColorStateList? = null
+
+    private var suffixText: String = ""
+    private var suffixTextAppearance = R.style.Taboo_TextAppearance_TabooEditText_Affix
+    private var suffixTextColor: ColorStateList? = null
+
+    private var hint: String = ""
+    private var inputType: Int = 0
+    private var text: String = ""
+
+    init {
+        val typed = context.obtainStyledAttributes(attrs, R.styleable.TabooEditText)
+        val prefixText = typed.getString(R.styleable.TabooEditText_prefixText) ?: ""
+        val prefixTextAppearance = typed.getResourceId(R.styleable.TabooEditText_prefixTextAppearance, R.style.Taboo_TextAppearance_TabooEditText_Affix)
+        val prefixTextColor = typed.getColorStateList(R.styleable.TabooEditText_prefixTextColor)
+
+        val suffixText = typed.getString(R.styleable.TabooEditText_suffixText) ?: ""
+        val suffixTextAppearance = typed.getResourceId(R.styleable.TabooEditText_suffixTextAppearance, R.style.Taboo_TextAppearance_TabooEditText_Affix)
+        val suffixTextColor = typed.getColorStateList(R.styleable.TabooEditText_suffixTextColor)
+
+        val hint = typed.getString(R.styleable.TabooEditText_android_hint) ?: ""
+        val inputType = typed.getInt(R.styleable.TabooEditText_android_inputType, 1)
+        val text = typed.getString(R.styleable.TabooEditText_android_text) ?: ""
+
+        typed.recycle()
+
+        setPrefixText(prefixText)
+        setPrefixTextAppearance(prefixTextAppearance)
+        setPrefixTextColor(prefixTextColor)
+
+        setSuffixText(suffixText)
+        setSuffixTextAppearance(suffixTextAppearance)
+        setSuffixTextColor(suffixTextColor)
+
+        setHint(hint)
+        setInputType(inputType)
+        setText(text)
+    }
+
+    fun setPrefixText(prefixText: String) {
+        this.prefixText = prefixText
+        updatePrefixText()
+    }
+
+    private fun updatePrefixText() {
+        binding.tvPrefix.text = prefixText
+        binding.tvPrefix.visibility = VISIBLE
+    }
+
+    fun setPrefixTextAppearance(prefixTextAppearance: Int) {
+        if (this.prefixTextAppearance != prefixTextAppearance) {
+            this.prefixTextAppearance = prefixTextAppearance
+            updatePrefixTextAppearance()
+        }
+    }
+
+    private fun updatePrefixTextAppearance() {
+        binding.tvPrefix.setTextAppearance(this.prefixTextAppearance)
+    }
+
+    fun setPrefixTextColor(prefixTextColor: ColorStateList?) {
+        if (prefixTextColor == null)
+            return
+
+        this.prefixTextColor = prefixTextColor
+        updatePrefixTextColor()
+    }
+
+    private fun updatePrefixTextColor() {
+        binding.tvPrefix.setTextColor(prefixTextColor)
+    }
+
+    fun setSuffixText(suffixText: String) {
+        this.suffixText = suffixText
+        updateSuffixText()
+    }
+
+    private fun updateSuffixText() {
+        binding.tvSuffix.text = suffixText
+        binding.tvSuffix.visibility = VISIBLE
+    }
+
+    fun setSuffixTextAppearance(prefixTextAppearance: Int) {
+        if (this.suffixTextAppearance != prefixTextAppearance) {
+            this.suffixTextAppearance = prefixTextAppearance
+            updateSuffixTextAppearance()
+        }
+    }
+
+    private fun updateSuffixTextAppearance() {
+        binding.tvSuffix.setTextAppearance(this.suffixTextAppearance)
+    }
+
+    fun setSuffixTextColor(suffixTextColor: ColorStateList?) {
+        if (suffixTextColor == null)
+            return
+
+        this.suffixTextColor = suffixTextColor
+        updateSuffixTextColor()
+    }
+
+    private fun updateSuffixTextColor() {
+        binding.tvSuffix.setTextColor(suffixTextColor)
+    }
+
+    fun setHint(hint: String) {
+        this.hint = hint
+        updateHint()
+    }
+
+    private fun updateHint() {
+        binding.edtText.hint = this.hint
+    }
+
+    fun setInputType(inputType: Int) {
+        this.inputType = inputType
+        updateInputType()
+    }
+
+    private fun updateInputType() {
+        binding.edtText.inputType = this.inputType
+    }
+
+    fun setText(text: String) {
+        if (this.text == text)
+            return
+
+        this.text = text
+        updateText()
+    }
+
+    private fun updateText() {
+        binding.edtText.setText(this.text)
+    }
+}
