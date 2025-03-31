39bd0e7ba8ac5f9437b439a6aa563cd936416f75
Han jun Kwon <hanjun.kwon.hs@gmail.com>
2025-03-31 09:15:30 +0900

    TABOO-79 TabooMenuButton > setOnCheckedChangeListener 메서드 추가

--- app/src/main/java/com/kwon/taboosample/PreviewButtonsActivity.kt
+++ app/src/main/java/com/kwon/taboosample/PreviewButtonsActivity.kt
@@ -1,10 +1,12 @@
 package com.kwon.taboosample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kwon.taboo.button.TabooMenuButton
 
 class PreviewButtonsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,16 @@ class PreviewButtonsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<TabooMenuButton>(R.id.tmb_language).setOnClickListener {
+            Log.d(">>>", "Language button clicked")
+        }
+        findViewById<TabooMenuButton>(R.id.tmb_theme).setOnCheckedChangeListener { _, isToggle ->
+            Log.d(">>>", "Theme button clicked: $isToggle")
+        }
+        findViewById<TabooMenuButton>(R.id.tmb_theme).setOnClickListener {
+            Log.d(">>>", "Theme button clicked")
         }
     }
 }
