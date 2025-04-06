1689e452f31d63305f871767019b709af1159cc2
hanjunkwon <hanjun.kwon@medisolution.co.kr>
2025-04-06 21:21:15 +0900

    Taboo Widget Maven 배포 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,14 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false// this version matches your Kotlin version
     id("com.android.library") version "8.8.1" apply false
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(System.getenv("mavenCentralUsername"))
+            password.set(System.getenv("mavenCentralPassword"))
+        }
+    }
 }
--- taboo-widget/build.gradle.kts
+++ taboo-widget/build.gradle.kts
@@ -1,11 +1,12 @@
 import java.util.Properties
-import org.gradle.api.publish.maven.MavenPublication
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     id("com.android.library")
     kotlin("android")
     id("maven-publish")
     id("signing")
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 val localProperties = Properties().apply {
@@ -23,7 +24,7 @@ val ossrhPassword: String? = localProperties.getProperty("ossrhPassword")
 
 
 group = "com.kwon.taboo"
-version = "1.0.0"
+version = "0.0.0"
 
 android {
     namespace = "com.kwon.taboo"
@@ -73,48 +74,6 @@ android {
     buildFeatures {
         buildConfig = true
     }
-
-    publishing {
-        singleVariant("release") // ✅ 이거 안 넣으면 components["release"] 못 씀!
-    }
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                from(components["release"])
-
-                groupId = "com.kwon.taboo"
-                artifactId = "taboo-widget"
-                version = "1.0.0"
-
-                pom {
-                    name.set("Taboo")
-                    description.set("UI Library for Android")
-                    url.set("https://github.com/HanJunKwon/Taboo")
-                    licenses {
-                        license {
-                            name.set("MIT License")
-                            url.set("https://opensource.org/licenses/MIT")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("copybara")
-                            name.set("Hanjun Kwon")
-                            email.set("skclaqks11@naver.com")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:https://github.com/HanJunKwon/Taboo.git")
-                        developerConnection.set("scm:git:https://github.com/HanJunKwon/Taboo.git")
-                        url.set("https://github.com/HanJunKwon/Taboo")
-                    }
-                }
-            }
-        }
-    }
 }
 
 dependencies {
@@ -129,4 +88,47 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+        // 프로젝트의 그룹 ID, 아티팩트 ID, 버전 설정
+    coordinates("com.kwon.taboo", "taboo-widget", "0.0.0")
+
+    // POM 정보 설정
+    pom {
+        name.set("Taboo")  // 라이브러리 이름
+        description.set("UI Labrary for Android")  // 라이브러리 설명
+        url.set("https://github.com/HanJunKwon/Taboo")  // 프로젝트 URL
+        inceptionYear.set("2025")  // 프로젝트 시작 연도
+
+        // 라이선스 정보 설정
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        // 개발자 정보 설정
+        developers {
+            developer {
+                id.set("copybara")                          // 개발자 ID
+                name.set("HanjunKwon")                      // 개발자 이름
+                url.set("https://github.com/HanJunKwon")    // 개발자 URL
+                email.set("skclaqks11@naver.com")           // 개발자 e-mail
+            }
+        }
+
+        // 소스 코드 관리(SCM) 정보 설정
+        scm {
+            url.set("https://github.com/HanJunKwon/Taboo")
+            connection.set("scm:git:https://github.com/HanJunKwon/Taboo.git")
+            developerConnection.set("scm:git:https://github.com/HanJunKwon/Taboo.git")
+        }
+    }
 }
