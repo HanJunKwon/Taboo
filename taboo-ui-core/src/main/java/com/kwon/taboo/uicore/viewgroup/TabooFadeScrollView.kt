51d7a95f1e4121dda34784d3aadd2c5ff8976825
Han jun Kwon <hanjun.kwon.hs@gmail.com>
2025-05-22 08:49:36 +0900

    TabooFadeScrollView > Fade View 높이 수정 로직 추가

--- taboo-ui-core/src/main/java/com/kwon/taboo/uicore/viewgroup/TabooFadeScrollView.kt
+++ taboo-ui-core/src/main/java/com/kwon/taboo/uicore/viewgroup/TabooFadeScrollView.kt
@@ -5,103 +5,144 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import android.widget.ScrollView
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.annotation.IntDef
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 import com.kwon.taboo.uicore.R
 import com.kwon.taboo.uicore.util.ResourceUtils
 
-class TabooFadeScrollView(context: Context, attrs: AttributeSet): ScrollView(context, attrs) {
+class TabooFadeScrollView(context: Context, attrs: AttributeSet): TabooScrollViewCore(context, attrs) {
+    @FadePosition
+    private var fadePosition = FADE_POSITION_BOTTOM
     private var fadeHeight = 0f
-    private var wrapper = ConstraintLayout(context).apply {
-        id = View.generateViewId()
-        layoutParams = ConstraintLayout.LayoutParams(
-            ConstraintLayout.LayoutParams.MATCH_PARENT,
-            ConstraintLayout.LayoutParams.MATCH_PARENT
-        )
-    }
-
-    private var contentContainer = LinearLayout(context).apply {
-        id = View.generateViewId()
-        layoutParams = ConstraintLayout.LayoutParams(
-            ConstraintLayout.LayoutParams.MATCH_PARENT,
-            ConstraintLayout.LayoutParams.MATCH_PARENT
-        )
-        orientation = LinearLayout.VERTICAL
-    }
 
     private var fadeContainer: LinearLayout = LinearLayout(context).apply {
         id = View.generateViewId()
-        layoutParams = ConstraintLayout.LayoutParams(
-            ConstraintLayout.LayoutParams.MATCH_PARENT,
-            ResourceUtils.dpToPx(context, 25f)
+        layoutParams = LayoutParams(
+            LayoutParams.MATCH_PARENT,
+            LayoutParams.WRAP_CONTENT
         )
         background = ContextCompat.getDrawable(context, R.drawable.gradient_scroll_view_fade)
         orientation = LinearLayout.VERTICAL
     }
 
     init {
-        setWillNotDraw(false)
+        super.addView(fadeContainer)
 
-        wrapper.addView(contentContainer)
-        wrapper.addView(fadeContainer)
+        val typed = context.obtainStyledAttributes(attrs, R.styleable.TabooFadeScrollView)
+        val fadePosition = typed.getInt(R.styleable.TabooFadeScrollView_fadePosition, FADE_POSITION_BOTTOM)
+        val fadeHeight = typed.getDimension(R.styleable.TabooFadeScrollView_fadeHeight, 30f)
+        typed.recycle()
 
-        ConstraintSet().apply {
-            clone(wrapper)
-            this.connect(
-                fadeContainer.id,
-                ConstraintSet.BOTTOM,
-                wrapper.id,
-                ConstraintSet.BOTTOM
-            )
-            applyTo(wrapper)
-        }
+        setFadePositionInternal(fadePosition)
+        setFadeHeightInternal(fadeHeight)
 
-        addView(wrapper)
+        draw()
     }
 
     override fun addView(child: View?) {
-        if (child == wrapper) {
+        if (child === fadeContainer || child === scrollView) {
             super.addView(child)
         } else {
-            contentContainer.addView(child)
+            scrollView.addView(child)
         }
     }
 
     override fun addView(child: View?, index: Int) {
-        if (child == wrapper) {
-            if (childCount > 0) {
-                return
-            } else {
-                super.addView(child, index)
-            }
+        if (child === fadeContainer || child === scrollView) {
+            super.addView(child, index)
         } else {
-            contentContainer.addView(child, index)
+            scrollView.addView(child, index)
         }
     }
 
     override fun addView(child: View?, params: ViewGroup.LayoutParams?) {
-        if (child == wrapper) {
-            if (childCount > 0) {
-                return
-            } else {
-                super.addView(child, params)
-            }
+        if (child === fadeContainer || child === scrollView) {
+            super.addView(child, params)
         } else {
-            contentContainer.addView(child, params)
+            scrollView.addView(child, params)
         }
     }
 
     override fun addView(child: View?, index: Int, params: ViewGroup.LayoutParams?) {
-        if (child == wrapper) {
-            if (childCount > 0) {
-                return
-            } else {
-                super.addView(child, index, params)
-            }
+        if (child === fadeContainer || child === scrollView) {
+            super.addView(child, index, params)
         } else {
-            contentContainer.addView(child, index, params)
+            scrollView.addView(child, index, params)
         }
+    }
+
+    private fun draw() {
+        updateFadePosition()
+        updateFadeHeight()
+    }
+
+    /**
+     * Todo 추후에 현재 스크롤뷰의 Orientation에 따라 [fadePosition] 설정의 예외처리를 해줘야함.
+     */
+    private fun setFadePositionInternal(@FadePosition fadePosition: Int) {
+        this.fadePosition = fadePosition
+    }
+
+    fun setFadePosition(@FadePosition fadePosition: Int) {
+        setFadePositionInternal(fadePosition)
+
+        updateFadePosition()
+    }
+
+    private fun updateFadePosition() {
+        ConstraintSet().apply {
+            clone(this@TabooFadeScrollView)
+            connect(
+                fadeContainer.id,
+                ConstraintSet.BOTTOM,
+                this@TabooFadeScrollView.id,
+                ConstraintSet.BOTTOM
+            )
+            applyTo(this@TabooFadeScrollView)
+        }
+    }
+
+    private fun setFadeHeightInternal(height: Float) {
+        fadeHeight = height
+    }
+
+    fun setFadeHeight(height: Float) {
+        setFadeHeightInternal(height)
+
+        updateFadeHeight()
+    }
+
+    private fun updateFadeHeight() {
+        fadeContainer.layoutParams.height = ResourceUtils.dpToPx(context, fadeHeight)
+        fadeContainer.requestLayout()
+    }
+
+    @IntDef(
+        FADE_POSITION_TOP,
+        FADE_POSITION_BOTTOM,
+        FADE_POSITION_BOTH
+    )
+    @Retention(AnnotationRetention.SOURCE)
+    annotation class FadePosition
+
+    companion object {
+        /**
+         * `orientation`이 `horizontal`일 때, **fade**의 위치를 **상단**에 설정.
+         */
+        const val FADE_POSITION_TOP = 0
+
+        /**
+         * `orientation`이 `horizontal`일 때, **fade**의 위치를 **하단**에 설정.
+         */
+        const val FADE_POSITION_BOTTOM = 1
+
+        /**
+         * `orientation`이 `horizontal`일 때, **fade**의 위치를 **시작**에 설정.
+         */
+        const val FADE_POSITION_BOTH = 2
+
+        const val TOP_FADE_VIEW_INDEX = 0
+        const val BOTTOM_FADE_VIEW_INDEX = 1
     }
 }
