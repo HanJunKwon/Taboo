6fc61a69ef3d4614619102e2aa616ddcd7f6c606
Han jun Kwon <hanjun.kwon.hs@gmail.com>
2025-07-10 14:10:06 +0900

    TABOO-110 TabooTabLayout > 해상도에 따라 Tab 텍스트 사이즈가 다르게 보이는 이슈 수정

--- taboo-widget/src/main/java/com/kwon/taboo/tabs/TabooTabLayout.kt
+++ taboo-widget/src/main/java/com/kwon/taboo/tabs/TabooTabLayout.kt
@@ -156,7 +156,7 @@ class TabooTabLayout(
     }
 
     fun setTabTextSize(@ComplexDimensionUnit unit: Int = TypedValue.COMPLEX_UNIT_SP, textSize: Float) {
-        tabTextSizePixel = if (unit == TypedValue.COMPLEX_UNIT_PX) ResourceUtils.spToPx(context, textSize) else textSize
+        tabTextSizePixel = if (unit == TypedValue.COMPLEX_UNIT_PX) textSize else ResourceUtils.spToPx(context, textSize)
         (adapter as TabooTabAdapter).setTabTextSize(tabTextSizePixel)
     }
 
