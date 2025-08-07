0a93e97f508e83fc14981967558f66e661ba60c8
Han jun Kwon <hanjun.kwon.hs@gmail.com>
2025-08-07 19:08:02 +0900

    taboo-ui-core, taboo-widget 버전 업데이트

--- taboo-widget/build.gradle.kts
+++ taboo-widget/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "io.github.hanjunkwon"
-version = "0.0.46"
+version = "0.0.47"
 
 android {
     namespace = "com.kwon.taboo"
@@ -66,7 +66,7 @@ mavenPublishing {
     signAllPublications()
 
     // 프로젝트의 그룹 ID, 아티팩트 ID, 버전 설정
-    coordinates("io.github.hanjunkwon", "taboo-widget", "0.0.46")
+    coordinates("io.github.hanjunkwon", "taboo-widget", "0.0.47")
 
     // POM 정보 설정
     pom {
