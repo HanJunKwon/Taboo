c51747309c86d08dc4f475faade2c9c0d6070aab
Han jun Kwon <hanjun.kwon.hs@gmail.com>
2025-10-22 07:54:34 +0900

    TabooToast > 리패키징

--- taboo-widget/src/main/java/com/kwon/taboo/toast/TabooToast.kt
+++ taboo-widget/src/main/java/com/kwon/taboo/toast/TabooToast.kt
@@ -1,4 +1,4 @@
-package com.kwon.taboo
+package com.kwon.taboo.toast
 
 import android.content.Context
 import android.graphics.drawable.Drawable
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.kwon.taboo.R
 
 class TabooToast(private val context: Context?) : Toast(context) {
     private val rootView = LayoutInflater.from(context).inflate(R.layout.taboo_toast, null)
